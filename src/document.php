dda79debc4afc082794776a6367a3196a74fe844
nathaniel1024 <98196791+Nathaniel1024@users.noreply.github.com>
2025-06-01 00:55:56 +0800

    document

--- src/document.php
+++ src/document.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard</title>
-  <link rel="stylesheet" href="css/transaction.css">
+  <link rel="stylesheet" href="css/document.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
