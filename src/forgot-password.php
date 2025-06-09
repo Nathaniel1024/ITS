3645582f005383f77fb123810c1bea353a6a7e29
nathaniel1024 <98196791+Nathaniel1024@users.noreply.github.com>
2025-06-09 20:19:14 +0800

    insert your smtp username and password here

--- src/forgot-password.php
+++ src/forgot-password.php
@@ -57,8 +57,8 @@ if (isset($_POST['email'])) {
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'pennywisesystem@gmail.com';                     //SMTP username
-            $mail->Password   = 'etjw iftt xqqs vwuv';                               //SMTP password
+            $mail->Username   = 'your username here';                     //SMTP username
+            $mail->Password   = 'your password here';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;           //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
